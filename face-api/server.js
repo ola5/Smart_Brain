f75243124921fd6b92fb42c35a1ffb11faf644a0
Ola Fawol <ola.fawole5@gmail.com>
2018-08-11 00:57:11 +0100

    Adding Smart Brian Project

---
+++ face-api/server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'smart_brain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+	// console.log(data);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+
+// root route --> Method => GET :: respond with success fail
+app.get('/', (req, res)=> {
+	res.send(database.users);
+})
+
+// signin route    --> Method => POST  :: respond with success or fail
+app.post('/signin', signin.handleSignin(db, bcrypt))
+// register route  --> Method => POST :: respond with new usr object
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+// profile/:userid --> Method => GET :: respond with user profile
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+// image/  --> Method => PUT :: respond with user object with count
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+// from the route image
+// database.users.forEach(user => {
+// 		if (user.id === req.body.id) {
+// 			user.entries++
+// 			res.json(user.entries);
+// 		} 
+// 	})
+// 	if (!found) { 
+// 		res.status(404).json('no such user');
+// 	}
+
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+
+app.listen(3000, ()=> {
+	console.log('App is running on port 3000');
+})
+
+
